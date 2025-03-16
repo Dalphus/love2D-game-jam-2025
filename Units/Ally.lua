e12e8cb479b7ee5b469bb8dd5c2742bfa593fd17
Connor Bramhall <41408864+Connorwb@users.noreply.github.com>
2025-03-16 14:29:43 -0400

    multinodes

--- Units/Ally.lua
+++ Units/Ally.lua
@@ -1,4 +1,5 @@
 require("Units.Unit")
+require("Units.Node")
 
 Ally = {}
 Ally.__index = Ally
@@ -6,12 +7,12 @@ setmetatable(Ally, Unit)
 
 function Ally:new(...)
   local ally = Unit:new(...)
-  ally.movement_node = {}
+  ally.movement_nodes = {}
   setmetatable(ally, self)
   return ally
 end
 
 function Ally:addMovementNode(_x, _y)
   print(string.format("Adding movement node at %d, %d", _x, _y))
-  self.movement_node = {x = _x, y = _y}
+  table.insert(self.movement_nodes, Node:new(_x, _y))
 end
