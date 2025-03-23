3ebd7b8faf7d8e4d3967fed5a8579847e141b961
Connor Bramhall <41408864+Connorwb@users.noreply.github.com>
2025-03-22 22:12:19 -0400

    simple persuit

--- Scenes/SceneOne.lua
+++ Scenes/SceneOne.lua
@@ -1,5 +1,6 @@
 require("Scenes.Scene")
 require("Units.Turret")
+require("Units.Patrol")
 
 SceneOne = {}
 SceneOne.__index = SceneOne
@@ -17,6 +18,11 @@ function SceneOne:load()
     ["Geraldo"] = Dummy:new( 100, 100, 50 )
   }
   enemies = {
-    [1] = Turret:new(500, 500, 25, -math.pi/2, 0, math.pi/2, 500)
+    [1] = Turret:new(500, 500, 25, -math.pi/2, 0, math.pi/2, 500),
+    [2] = Patrol:new(600, 600, 15, -math.pi/2, 0, math.pi/2, 300)
   }
+  enemies[2]:addMovementNode(600, 600)
+  enemies[2]:addMovementNode(600, 700)
+  enemies[2]:addMovementNode(700, 700)
+  enemies[2]:addMovementNode(700, 600)
 end
